a5f2c4163d018a95aa77c9e709a13ea358bb40c3
Ahi <a362349247@vip.qq.com>
2024-05-08 14:46:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import os
 os.system('rm -rf poc clone-templates')
 os.system('python 1-clone_repos.py')
 os.system('python 2-remove_duplicated.py')
+os.system('python 2-1-remove_duplicated.py')
 os.system('python 3-get_pocname.py')
 
