f993482b8cf65599a3b3f896bd5e77a718f217c5
AdySec <133128400+adysec@users.noreply.github.com>
2024-07-02 12:48:15 +0800

    Update 4-check_poc.py

--- 4-check_poc.py
+++ 4-check_poc.py
@@ -5,14 +5,14 @@ POC_DIR = "poc"
 yaml_files = [f for f in os.listdir(POC_DIR) if f.endswith('.yaml') or f.endswith('.yml')]
 
 for file in yaml_files:
-    file_path = os.path.join(POC_DIR, file)
-    print(f"检查POC {file_path} 中...")
-    
-    command = f"./nuclei -t {file_path} -silent"
-    return_code = os.system(command)
-    
-    if return_code == 0:
-        print(f"{file_path}格式有效")
-    else:
-        print(f"{file_path}格式无效，已删除")
+	file_path = os.path.join(POC_DIR, file)
+	print(f"检查POC {file_path} 中...")
+	
+	command = f"./nuclei -t {file_path} -silent"
+	return_code = os.system(command)
+	
+	if return_code == 0:
+		print(f"{file_path}格式有效")
+	else:
+		print(f"{file_path}格式无效，已删除")
 		os.remove(file_path)
