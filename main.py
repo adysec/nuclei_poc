6e09ec395b218e5fb38cdafcd22e4738e95790ea
AdySec <133128400+adysec@users.noreply.github.com>
2025-01-21 17:34:41 +0800

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.system('rm -rf poc clone-templates')
-os.system('python 1-clone_repos.py')
-os.system('python 3-check_poc.py')
-os.system('python 4-remove_duplicated.py')
-os.system('python 5-get_pocname.py')
-
