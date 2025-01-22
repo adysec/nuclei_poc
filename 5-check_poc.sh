0b1a73deae1f01a5c2ac08d8af2bf6c83dadfc2e
AdySec <133128400+adysec@users.noreply.github.com>
2025-01-22 16:20:13 +0800

    Update 5-check_poc.sh

--- 5-check_poc.sh
+++ 5-check_poc.sh
@@ -23,6 +23,7 @@ fi
 yaml_files=$(find "$TMP_DIR" -type f \( -name "*.yml" -o -name "*.yaml" \))
 if [ -z "$yaml_files" ]; then
   echo "未找到 YAML 文件，退出。"
+  rm -rf "$TMP_DIR" && echo "TMP 目录已删除。"
   exit 0
 fi
 
@@ -41,7 +42,13 @@ while IFS= read -r -d '' file; do
   result=$(./nuclei -t "$file" -silent 2>&1)
   if echo "$result" | grep -q "FTL"; then
     echo "检查 POC 格式无效，已删除 $file"
-    rm -f "$file" || echo "无法删除 $file，跳过。"
+    
+    # 删除文件，处理特殊字符问题
+    if [ -n "$file" ]; then
+      rm -- "$file" && echo "已删除 $file" || echo "无法删除 $file，跳过。"
+    else
+      echo "文件名无效，跳过删除操作。"
+    fi
   else
     # 获取相对路径并创建目标目录
     dest_file="$POC_DIR/$(echo "$file" | sed "s|$TMP_DIR/||")"
@@ -57,5 +64,10 @@ while IFS= read -r -d '' file; do
     mv "$file" "$dest_file" && echo "已将文件 $file 移动至 $dest_file"
   fi
 done < <(find "$TMP_DIR" -type f \( -name "*.yml" -o -name "*.yaml" \) -print0)
+
+# 检查 TMP 目录是否为空并删除
+if [ -z "$(find "$TMP_DIR" -type f)" ]; then
+  rm -rf "$TMP_DIR" && echo "TMP 目录已删除。"
+fi
 
 echo "POC 检查完成。"
