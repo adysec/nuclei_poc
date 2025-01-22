02d2c32459d943d73fa74fc183fedab7eafbcb3e
AdySec <133128400+adysec@users.noreply.github.com>
2025-01-22 17:36:02 +0800

    Update 5-check_poc.py

--- 5-check_poc.py
+++ 5-check_poc.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import subprocess
 import time
+import hashlib
 
 POC_DIR, TMP_DIR, TIMEOUT, START_TIME = "poc", "tmp", 19800, time.time()
 
@@ -34,6 +35,14 @@ def check_yaml_format(file_path):
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     return "FTL" not in result.stdout and "FTL" not in result.stderr
 
+def get_file_hash(file_path):
+    """计算文件的hash值"""
+    hash_sha256 = hashlib.sha256()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
+
 ensure_dir(POC_DIR)
 if not os.path.exists(TMP_DIR):
     print("tmp/ 目录不存在，退出。")
@@ -46,14 +55,26 @@ if not yaml_files:
     print("tmp/ 目录已删除。")
     exit(0)
 
+# 存储已处理文件的哈希值
+processed_files_hash = {}
+
 for file_path in yaml_files:
     if time.time() - START_TIME >= TIMEOUT:
         print("运行时间已超过 5 小时 30 分钟，强制退出。")
         exit(0)
 
+    file_hash = get_file_hash(file_path)
+    
+    # 如果该文件的hash已经处理过，则删除该文件
+    if file_hash in processed_files_hash:
+        safe_remove(file_path)
+        continue
+
     if not check_yaml_format(file_path):
         safe_remove(file_path)
     else:
+        # 记录当前文件的hash值
+        processed_files_hash[file_hash] = file_path
         move_file(file_path, os.path.join(POC_DIR, os.path.relpath(file_path, TMP_DIR)))
 
 if not any(os.scandir(TMP_DIR)):
