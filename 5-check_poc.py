01e565a498a3a6c3501063887fa13294f8127057
AdySec <133128400+adysec@users.noreply.github.com>
2025-01-22 17:24:02 +0800

    Update 5-check_poc.py

--- 5-check_poc.py
+++ 5-check_poc.py
@@ -11,12 +11,23 @@ def ensure_dir(directory):
 def safe_remove(file_path):
     if os.path.exists(file_path):
         os.remove(file_path)
-        print(f"已删除 {file_path}")
+        print(f"poc校验失败，已删除文件: {file_path}")
 
 def move_file(src, dest):
     os.makedirs(os.path.dirname(dest), exist_ok=True)
-    shutil.move(src, dest)
-    print(f"已将文件 {src} 移动至 {dest}")
+    try:
+        if os.path.exists(dest):
+            base, ext = os.path.splitext(dest)
+            counter = 1
+            new_dest = f"{base}_{counter}{ext}"
+            while os.path.exists(new_dest):
+                counter += 1
+                new_dest = f"{base}_{counter}{ext}"
+            dest = new_dest
+        shutil.move(src, dest)
+        print(f"poc校验成功，已移动文件: {src} -> {dest}")
+    except Exception as e:
+        print(f"移动文件出错: {src} -> {dest}, 错误: {e}")
 
 def check_yaml_format(file_path):
     result = subprocess.run(["./nuclei", "-t", file_path, "-silent"],
@@ -25,14 +36,14 @@ def check_yaml_format(file_path):
 
 ensure_dir(POC_DIR)
 if not os.path.exists(TMP_DIR):
-    print("TMP 目录不存在，退出。")
+    print("tmp/ 目录不存在，退出。")
     exit(1)
 
 yaml_files = [os.path.join(root, file) for root, _, files in os.walk(TMP_DIR)
               for file in files if file.endswith(('.yml', '.yaml'))]
 if not yaml_files:
     shutil.rmtree(TMP_DIR, ignore_errors=True)
-    print("TMP 目录已删除。")
+    print("tmp/ 目录已删除。")
     exit(0)
 
 for file_path in yaml_files:
@@ -41,13 +52,12 @@ for file_path in yaml_files:
         exit(0)
 
     if not check_yaml_format(file_path):
-        print(f"检查 POC 格式无效，已删除 {file_path}")
         safe_remove(file_path)
     else:
         move_file(file_path, os.path.join(POC_DIR, os.path.relpath(file_path, TMP_DIR)))
 
 if not any(os.scandir(TMP_DIR)):
     shutil.rmtree(TMP_DIR, ignore_errors=True)
-    print("TMP 目录已删除。")
+    print("tmp/ 目录已删除。")
 
 print("POC 检查完成。")
